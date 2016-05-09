3fc2eef4fb9a58561065571134a4897271febf3a
Vyacheslav Kurochkin <VKurochkin@epicloansystems.com>
2016-05-09 11:49:49 -0400

    I feel confident enough that equals() works, except for reduce

--- src/test/java/p1/FractionEqualsTest.java
+++ src/test/java/p1/FractionEqualsTest.java
@@ -3,6 +3,7 @@ package p1;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by VKurochkin on 5/9/2016.
@@ -13,4 +14,29 @@ public class FractionEqualsTest {
     public void sameNumeratorAndDenominator() throws Exception {
         assertEquals(new Fraction(3,5), new Fraction(3,5));
     }
+
+    @Test
+    public void differentNumerators() throws Exception {
+        assertNotEquals(new Fraction(1, 5), new Fraction(2, 5));
+    }
+
+    @Test
+    public void differentDenominators() throws Exception {
+        assertNotEquals(new Fraction(3, 4), new Fraction(3,7));
+    }
+
+    @Test
+    public void wholeNumberEqualsSameFraction() throws Exception {
+        assertEquals(new Fraction(5, 1), new Fraction(5));
+    }
+
+    @Test
+    public void wholeNumberNotEqualsToDifferentWholeNumber() throws Exception {
+        assertNotEquals(new Fraction(6), new Fraction(5));
+    }
+
+//    @Test
+//    public void reduce() throws Exception{
+//        assertEquals(new Fraction(3, 4), new Fraction(6, 8));
+//    }
 }
